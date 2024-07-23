1e51ae5fbedbb91179912550e498893b167efeb5
LucasNagel <lucasanagel@gmail.com>
2024-07-23 20:18:55 -0300

    bugfix de un error en la ruta que traia los componentes a la screen de workspace

--- src/Screens/Workspace/Workspace.jsx
+++ src/Screens/Workspace/Workspace.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { Aside, Channels, Content, DirectMessages, Header } from '/home/lucas/Slack Clon/Slack/src/Components/Workspace/index.js'
+import { Aside, Channels, Content, DirectMessages, Header } from '../../Components/Workspace/index.js'
 
 export const Workspace = () => {
     const { id } = useParams()
