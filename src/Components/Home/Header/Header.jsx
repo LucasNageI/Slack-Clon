3a14a37221ffb8be3887e2d50a60f6d6862724a1
LucasNagel <lucasanagel@gmail.com>
2024-07-28 16:47:19 -0300

    hamburguer menu terminado

--- src/Components/Home/Header/Header.jsx
+++ src/Components/Home/Header/Header.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from 'react'
-import "./Header.css"
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
-    const [isOpen, setChange] = useState(false)
+    const [isOpen, setChange] = useState(false);
 
     const handleHamMenu = () => {
-        setChange(!isOpen)
-    }
+        setChange(!isOpen);
+    };
 
     return (
-        <header className={`header ${isOpen ? 'menu-open' : 'menu-closed'}`}>
+        <header className='header'>
             <nav className='nav'>
-                    <div className={`nav-list ${isOpen ? 'open' : 'closed'}`}>
-                        <img src='https://a.slack-edge.com/3d92b39/marketing/img/nav/slack-salesforce-logo-nav-white.png' alt='slack icon' />
-                        <ul className={`list-container ${isOpen ? 'open' : 'closed'}`}>
-                            <li></li>
-                            <li><button className='btn'>Features <i className="bi bi-chevron-down"></i></button></li>
-                            <li><button className='btn'>Solutions <i className="bi bi-chevron-down"></i></button></li>
-                            <li><button className='btn'>Enterprise</button></li>
-                            <li><button className='btn'>Resources <i className="bi bi-chevron-down"></i></button></li>
-                            <li><button className='btn'>Pricing</button></li>
-                        </ul>
-                    </div>
-                <div className='ham-menu-container btn-container'>
-                    <button onClick={handleHamMenu} className={`navbar-btn ${isOpen ? 'close-ham-menu' : 'open-ham-menu'}`}>
-                            <i className={`bi ${isOpen ? 'bi-x' : 'bi-list'}`}></i>
-                        </button>
-                    <ul className='btn-list'>
-                        <li className='search'><button className='btn'><i className="bi bi-search"></i></button></li>
-                        <li><button className='btn talk-to-sales'>TALK TO SALES</button></li>
-                        <li><Link to={"/CreateWorkspace"} className='create-workspace-btn'>CREATE A NEW WORKSPACE</Link></li>
+                <div className={`nav-list ${isOpen ? 'open' : 'closed'}`}>
+                    <img src='https://a.slack-edge.com/3d92b39/marketing/img/nav/slack-salesforce-logo-nav-white.png' alt='slack icon' />
+                    <ul className={`list-container ${isOpen ? 'open' : 'closed'}`}>
+                        <li></li>
+                        <li><button className={`li-btn ${isOpen ? 'open' : 'closed'}`}>Features <i className="bi bi-chevron-down"></i></button></li>
+                        <li><button className={`li-btn ${isOpen ? 'open' : 'closed'}`}>Solutions <i className="bi bi-chevron-down"></i></button></li>
+                        <li><button className={`li-btn ${isOpen ? 'open' : 'closed'}`}>Enterprise</button></li>
+                        <li><button className={`li-btn ${isOpen ? 'open' : 'closed'}`}>Resources <i className="bi bi-chevron-down"></i></button></li>
+                        <li><button className={`li-btn ${isOpen ? 'open' : 'closed'}`}>Pricing</button></li>
                     </ul>
+                </div>
+                <div className='btn-container'>
+                    <button className='search btn'>
+                        <i className="bi bi-search"></i>
+                    </button>
+                    <button onClick={handleHamMenu} className={`navbar-btn ${isOpen ? 'close-ham-menu' : 'open-ham-menu'}`}>
+                        <i className={`bi ${isOpen ? 'bi-x' : 'bi-list'}`}></i>
+                    </button>
+                    <div className='btn-list'>
+                        <button className='btn talk-to-sales'>TALK TO SALES</button>
+                        <Link to={"/CreateWorkspace"} className='btn create-workspace-btn'>CREATE A NEW WORKSPACE</Link>
+                    </div>
                 </div>
             </nav>
         </header>
